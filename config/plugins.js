9e744df0f324d43f532e984455e56b0c37eff8d9
yasir <yasiriqbal1990@gmail.com>
2023-01-25 03:47:00 +0500

    added blog

--- config/plugins.js
+++ config/plugins.js
@@ -18,4 +18,16 @@ module.exports = ({ env }) => ({
         },
       },
     },
+  slugify: {
+    enabled: true,
+    config: {
+      contentTypes: {
+        article: {
+          field: 'slug',
+          references: 'title',
+        },
+      },
+    },
+  },
+
   });
